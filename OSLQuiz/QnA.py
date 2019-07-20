990a42ff2e65817ec28a515539ecbc23b1b54c67
Qasim <qasim.bhatia@gmail.com>
2019-07-20 20:09:59 +0530

    Added more Questions to file QnA class modified to deal with zero questions and changes qmax definition to include last question in file

--- OSLQuiz/QnA.py
+++ OSLQuiz/QnA.py
@@ -10,21 +10,24 @@ class QnA:
         with open(fname, encoding="utf8") as f:
             for i, l in enumerate(f):
                 pass
-        return i + 1
+        return i + 2
 
     def __init__(self):
         self.qmax = QnA.filelen('../QnA/questions.txt')
 
     def getquestion(self):
-        qnumber = random.randrange(0, self.qmax, 1)
+        if (self.qmax == 0):
+            return 0
+        qnumber = random.randrange(1, self.qmax, 1)
         question = linecache.getline('../QnA/questions.txt', qnumber)[:-1]
         answers = linecache.getline('../QnA/answers.txt', qnumber)[:-1].split(',')
-        return question, answers
+        return question, answers, qnumber
 
 
 test = QnA()
 print(test.qmax)
 question = test.getquestion()
+print(question[2])
 print(question[0])
 print(question[1])
 
