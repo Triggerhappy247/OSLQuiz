fa4f50719e5f4a9ac4ca22b7c41d328c1f266228
Qasim <qasim.bhatia@gmail.com>
2019-07-20 18:55:39 +0530

    QnA Class ready - Fetches a random question from the question file and its options from the answers file

--- OSLQuiz/QnA.py
+++ OSLQuiz/QnA.py
@@ -1,4 +1,5 @@
 import linecache
+import random
 
 
 class QnA:
@@ -14,6 +15,16 @@ class QnA:
     def __init__(self):
         self.qmax = QnA.filelen('../QnA/questions.txt')
 
+    def getquestion(self):
+        qnumber = random.randrange(0, self.qmax, 1)
+        question = linecache.getline('../QnA/questions.txt', qnumber)[:-1]
+        answers = linecache.getline('../QnA/answers.txt', qnumber)[:-1].split(',')
+        return question, answers
+
 
 test = QnA()
 print(test.qmax)
+question = test.getquestion()
+print(question[0])
+print(question[1])
+
