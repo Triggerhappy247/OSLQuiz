ebb2f182a105473e4924ea82c6fc8a3771e0370b
Qasim <qasim.bhatia@gmail.com>
2019-07-20 17:47:23 +0530

    Added event handling.

--- OSLQuiz/Quiz.py
+++ OSLQuiz/Quiz.py
@@ -1,5 +1,10 @@
 import tkinter
 
+def destroyWindow(event):
+    introLabel.destroy()
+    startButton.destroy()
+    mainWindow.destroy()
+
 mainWindow = tkinter.Tk()
 mainWindow.geometry('500x500+250+250')
 mainWindow.title('Q&J Quiz')
@@ -7,4 +12,5 @@ introLabel = tkinter.Label(mainWindow,text='Welcome to this Quiz, Press Start!')
 startButton = tkinter.Button(mainWindow,text='START')
 introLabel.pack()
 startButton.pack()
+startButton.bind('<Button-1>',destroyWindow)
 mainWindow.mainloop()
