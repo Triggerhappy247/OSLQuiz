97bde7df403a7ac16af6d9d2a47528b497cd7f05
Qasim <qasim.bhatia@gmail.com>
2019-07-20 17:33:56 +0530

    Added a Label,Start Button, and changed the window size.

--- OSLQuiz/Quiz.py
+++ OSLQuiz/Quiz.py
@@ -1,5 +1,10 @@
 import tkinter
 
 mainWindow = tkinter.Tk()
+mainWindow.geometry('500x500+250+250')
 mainWindow.title('Q&J Quiz')
+introLabel = tkinter.Label(mainWindow,text='Welcome to this Quiz, Press Start!')
+startButton = tkinter.Button(mainWindow,text='START')
+introLabel.pack()
+startButton.pack()
 mainWindow.mainloop()
